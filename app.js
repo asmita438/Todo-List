823bb0d0fa836544318c09d2c56480640dc2278c
Asmita Mallik <asmita438@gmail.com>
2020-08-19 20:33:08 +0545

    added event-listener

---
+++ app.js
@@ -0,0 +1,17 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+
+//Event Listener
+todoButton.addEventListener('click', addTodo);
+
+
+//Functions
+function addTodo(event){
+    
+    
+    }
+
